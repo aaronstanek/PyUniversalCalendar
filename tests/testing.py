37ae259f952cd5e91e82b0811cd2da588b9273bc
Aaron Stanek <amr.stanek@gmail.com>
2021-10-06 10:57:49 -0700

    added basic tests

--- tests/testing.py
+++ tests/testing.py
@@ -3,3 +3,28 @@ import sys
 sys.path.append("src")
 
 import PyUniversalCalendar as puc
+
+class TestErrorClasses(unittest.TestCase):
+
+    def test_a(self):
+        self.assertTrue(isinstance(
+            puc.UniversalCalendarError(),
+            Exception
+            ))
+        self.assertTrue(isinstance(
+            puc.UniversalCalendarValidationError(),
+            puc.UniversalCalendarError
+            ))
+        self.assertTrue(isinstance(
+            puc.UniversalCalendarBoundsError(),
+            puc.UniversalCalendarError
+            ))
+
+class TestBaseClass(unittest.TestCase):
+
+    def test_a(self):
+        with self.assertRaises(NotImplementedError):
+            puc.UniversalCalendarDate()
+
+if __name__ == '__main__':
+    unittest.main()
